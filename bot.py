7807a65029f7bea71b5358cf081f046b50b93e3c
BetaCrasher <khurramkhan123456789@gmail.com>
2020-05-18 19:32:22 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Bot is online')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if str(message.channel) == 'the-general-space' and 'twitch.tv/' in message.content:
+        await message.channel.purge(limit=1)
+        await message.channel.send('Pls post links in their respective advertise channels. Thank you')
+
+    if message.content.startswith('$purge') and str(message.author) == 'BetaCrasher#6993':
+        await message.channel.purge()
+
+client.run('NzEyMDQ0MDY4MDU3OTA3MjYx.XsL0zQ.dlE3ftN604JziBIEo2Ijy3Ucsns')
